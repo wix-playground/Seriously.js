8b07aee6247fa2f415d0390e6b7ee1f477006c9a
Yehonatan Daniv <yehonatand@wix.com>
2017-09-18 11:10:18 +0300

    Added babel transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,25 @@ import progress from 'rollup-plugin-progress';
 import filesize from 'rollup-plugin-filesize';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
+import babel from 'rollup-plugin-babel';
 
 export default {
 	entry: 'index.js',
 	targets: [{
 		dest: 'seriously.js',
 		format: 'amd',
-		name: 'Seriously',
-		amd: {
-			id: 'seriously'
-		}
+		name: 'Seriously'
 	}],
+	intro: 'if (typeof window !== "undefined") {',
+	outro: '}',
 	plugins: [
 		progress({
 			clearLine: false
 		}),
-		uglify({}, minify),
+		babel({
+			exclude: 'node_modules/**'
+		}),
+		// uglify({}, minify),
 		filesize()
 	]
 };
